921e351dae874653cfbc5a2a3e18b71c0f5ba665
Marcin Lech <arcinech@gmail.com>
2022-09-23 20:10:48 +0200

    Update: product controller and service

--- src/products/products-data.service.ts
+++ src/products/products-data.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { Product } from '../products/interfaces/product.inteface';
+import { UpdateProductDTO } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsDataService {
   private _products_: Product[] = [];
 
-  addProduct(newProduct: CreateProductDTO) {
+  addProduct(newProduct: CreateProductDTO): Product {
     const product: Product = {
       id: this._products_.length.toString(),
       ...newProduct,
@@ -14,5 +15,35 @@ export class ProductsDataService {
       updatedAt: new Date(),
     };
     this._products_.push(product);
+
+    return product;
+  }
+
+  updateProduct(id: string, product: UpdateProductDTO): Product {
+    this._products_.map((item) => {
+      if (item.id === id) {
+        return {
+          ...product,
+          id: item.id,
+          createdAt: item.createdAt,
+          updatedAt: new Date(),
+        };
+      }
+      return item;
+    });
+
+    return this.getProductById(id);
+  }
+
+  getAllProducts(): Array<Product> {
+    return this._products_;
+  }
+
+  getProductById(id: string): Product {
+    return this._products_.find((item) => item.id === id);
+  }
+
+  deleteProductById(id: string): void {
+    this._products_.filter((item) => item.id !== id);
   }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,32 +6,17 @@ import {
   Delete,
   Put,
   Body,
+  HttpCode,
 } from '@nestjs/common';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { ExternalProductDto } from './dto/external-product.dto';
 import { ProductsDataService } from './products-data.service';
 import { Product } from './interfaces/product.inteface';
-import { dateToArray, arrayToDate } from '../helpers/date.helper';
+import { dateToArray } from '../helpers/date.helper';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productRepository: ProductsDataService) {}
-  @Get(':id')
-  getProductById(@Param('id') _id_: string): string {
-    return `GetById ${_id_}`;
-  }
-
-  @Get()
-  getAllProducts(): string {
-    return 'GetAll';
-  }
-
-  @Post()
-  addIProduct(@Body() _product_: CreateProductDTO): ExternalProductDto {
-    return this.mapProductToExternal(
-      this.productRepository.addProduct(_product_),
-    );
-  }
 
   mapProductToExternal(product: Product): ExternalProductDto {
     return {
@@ -41,9 +26,31 @@ export class ProductsController {
     };
   }
 
+  @Get()
+  getAllProducts(): Array<ExternalProductDto> {
+    return this.productRepository
+      .getAllProducts()
+      .map((item) => this.mapProductToExternal(item));
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') _id_: string): ExternalProductDto {
+    return this.mapProductToExternal(
+      this.productRepository.getProductById(_id_),
+    );
+  }
+
+  @Post()
+  addIProduct(@Body() _product_: CreateProductDTO): ExternalProductDto {
+    return this.mapProductToExternal(
+      this.productRepository.addProduct(_product_),
+    );
+  }
+
   @Delete(':id')
-  deleteProduct(@Param('id') _id_: string): string {
-    return `Delete ${_id_}`;
+  @HttpCode(204)
+  deleteProduct(@Param('id') _id_: string): void {
+    this.productRepository.deleteProductById(_id_);
   }
 
   @Put(':id')
@@ -51,6 +58,8 @@ export class ProductsController {
     @Param('id') _id_: string,
     @Body() _product_: CreateProductDTO,
   ): ExternalProductDto {
-    return null;
+    return this.mapProductToExternal(
+      this.productRepository.updateProduct(_id_, _product_),
+    );
   }
 }
