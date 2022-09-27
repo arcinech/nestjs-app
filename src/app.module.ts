58fdaa2eb030434d66178c5af521c3dcfe755cda
Marcin Lech <arcinech@gmail.com>
2022-09-27 14:21:32 +0200

    Add: typeORM support for app and products

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+
+import config = require('./ormconfig');
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    TypeOrmModule.forRoot(config as DataSourceOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsDataService } from './products-data.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagRepository } from './db/tag.repository';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsDataService],
+  imports: [TypeOrmModule.forFeature([TagRepository])],
 })
 export class ProductsModule {}
