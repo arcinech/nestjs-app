7fe8f757dac55e76e5ab168ec384696878197dff
Marcin Lech <arcinech@gmail.com>
2022-09-28 14:30:55 +0200

    Add: imports for repositories in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersDataService } from './users-data.service';
+import { UserRepository } from './db/user.repository';
+import { UserAddressRepository } from './db/userAddress.repository';
 import { UserValidatorService } from './user-validator.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersDataService, UserValidatorService],
+  imports: [
+    TypeOrmModule.forFeature([UserAddressRepository]),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
 })
 export class UsersModule {}
