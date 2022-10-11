a968c17ad8c450dca420778e23f31ffb3fc77a6f
Marcin Lech <arcinech@gmail.com>
2022-10-11 22:16:06 +0200

    fix: naming in dto

--- src/orders/dto/external-order.dto.ts
+++ src/orders/dto/external-order.dto.ts
@@ -1,15 +1,18 @@
+import { UserAddress } from '../../users/db/userAddress.entity';
 import { Status } from '../enums/status.enums';
 
 export class ExternalOrderDto {
   id: string;
   orderItems: ExternalOrderItemDto[];
   status: Status;
-  total: number;
   additionalInfo: string;
-  user_firstname: string;
-  user_lastname: string;
-  user_email: string;
-  user_address: string;
+  userFirstName: string;
+  userLastName: string;
+  userEmail: string;
+  userAddress: UserAddress;
+  createdAt: number[];
+  updatedAt: number[];
+  total: number;
 }
 
 export class ExternalOrderItemDto {
